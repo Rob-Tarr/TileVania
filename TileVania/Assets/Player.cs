ec097d84100ed77ab0a47f0d8a990e62945cdce0
Robert Tarr <robtarr1@gmail.com>
2018-10-22 11:58:34 -0700

    Added crossplatform input asset. Created running states and animations. Got player moving along X axis

--- TileVania/Assets/Player.cs
+++ TileVania/Assets/Player.cs
@@ -1,16 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+
+    //Config
+    [SerializeField] float runSpeed = 5f;
+    
+    // State
+    bool isAlive = true;
+
+    // Cached References
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        Run();
+        FlipSprite();
 	}
+
+   private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 and +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+
+        }
+    }
 }
